d9640fb7a405568b4fb8524bb6a6eba5e9d64b1c
ivo-petronio <scytheratk@gmail.com>
2026-05-08 20:20:34 -0300

    feat: turning item into component and providing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Bag from './components/Bag.jsx'
+import Item from './components/Item.jsx'
 
 function App() {
 
@@ -97,25 +98,10 @@ function App() {
 
                 {
                     items.map(item => 
-                        <div 
+                        <Item
                             key={item.id}
-                            className={`product ${item.isInBag ? 'selected' : ''}`}>
-                            <div className="photo">
-                                <img src={"./img/" + item.photo} />
-                            </div>
-                            <div className="description">
-                                <span className="name">{item.name}</span>
-                                <span className="price">$ {item.price}</span>
-
-                                {item.isInBag &&
-                                    <div className="quantity-area">
-                                        <button>-</button>
-                                        <span className="quantity">{item.quantity}</span>
-                                        <button>+</button>
-                                    </div>
-                                }
-                            </div>
-                        </div>
+                            item={item}
+                        />
                     )
                 }
             </section>
